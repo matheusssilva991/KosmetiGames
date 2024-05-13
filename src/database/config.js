888854fa147a015fad4875e63af6729b87d1c7f1
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-05-13 18:26:02 -0300

    fix: docker file + docker compose + env.example

--- src/database/config.js
+++ src/database/config.js
@@ -6,7 +6,7 @@ const config = {
   db: {
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_ROOT_PASSWORD || '',
+    password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'test',
     port: process.env.MYSQL_PORT || 3306,
   },
