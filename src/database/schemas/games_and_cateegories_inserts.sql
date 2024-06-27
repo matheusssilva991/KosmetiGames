1efea73e9f467514ac6910305d74fbb40e55c98a
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-06-26 21:16:32 -0300

    update: criando script sql para inicializar as tabelas mages & categories

---
+++ src/database/schemas/games_and_cateegories_inserts.sql
@@ -0,0 +1,22 @@
+-- INSERTS FOR game TABLE
+
+INSERT INTO game (name, enterprise) VALUES ('League of Legends', 'RIOT Games');
+INSERT INTO game (name, enterprise) VALUES ('DOTA 2', 'VALVE');
+INSERT INTO game (name, enterprise) VALUES ('VALORANT', 'RIOT Games');
+INSERT INTO game (name, enterprise) VALUES ('Overwatch 2', 'Blizzard Entertainment');
+INSERT INTO game (name, enterprise) VALUES ('Overwatch', 'Blizzard Entertainment');
+INSERT INTO game (name, enterprise) VALUES ('Paladins', 'Hi Rez Studios');
+
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+
+-- INSERTS FOR category TABLE
+
+INSERT INTO category (name) VALUES ('Skin');
+INSERT INTO category (name) VALUES ('Passe de Batalha');
+INSERT INTO category (name) VALUES ('Pack');
+INSERT INTO category (name) VALUES ('Acessorio');
+INSERT INTO category (name) VALUES ('Cama, mesa e banho');
+INSERT INTO category (name) VALUES ('Roupa');
+INSERT INTO category (name) VALUES ('Cosmetico');
